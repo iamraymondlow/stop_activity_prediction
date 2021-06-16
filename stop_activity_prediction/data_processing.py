ddac976dc24f8ed99842439cbfad51089b38823c
Raymond Low <iamraymondlow@gmail.com>
2021-06-17 00:30:59 +0800

    included functions to load verified trips and operation survey and merge them

--- stop_activity_prediction/data_processing.py
+++ stop_activity_prediction/data_processing.py
@@ -16,28 +16,48 @@ class DataProcessor:
     def __init__(self):
         self.combined_data = pd.DataFrame()
 
-    def _load_verified_stops(self, batch_num):
-        return None
+    def _load_verified_trips(self, batch_num):
+        with open(config['verified_stop_directory'].format(batch_num=batch_num)) as f:
+            verified_trips = json.load(f)
+        verified_trips = pd.json_normalize(verified_trips)
+
+        # filter important features
+        retained_columns = ['DriverID', 'VehicleType', 'Stops', 'Travels', 'YMD', 'Timeline', 'DayOfWeekStr']
+
+        return verified_trips[retained_columns]
 
     def _load_operational_survey(self, batch_num):
-        return None
+        # load operational survey
+        with open(config['operation_survey_directory'].format(batch_num=batch_num)) as f:
+            operational_data = json.load(f)
+        operational_data = pd.json_normalize(operational_data)
 
-    def _load_vehicle_info(self, batch_num):
+        # filter important features
+        important_features = ['FrequentPlaces', 'Commodity', 'SpecialCargo', 'Company.Type', 'Industry',
+                              'Driver.ID']
+        retained_columns = [column
+                            for column in operational_data.columns
+                            for feature in important_features
+                            if feature in column]
+
+        return operational_data[retained_columns]
+
+    def _extract_verified_stops(self, verified_trips):
         return None
 
     def process_data(self, batch_num):
-        # import verified stop information
-        verified_stops = self._load_verified_stops(batch_num)
+        # import verified trip information
+        verified_trips = self._load_verified_trips(batch_num)
 
         # import operational survey data
         operational_data = self._load_operational_survey(batch_num)
 
-        # import vehicle information
-        vehicle_information = self._load_vehicle_info(batch_num)
+        # extract verified stop information
+        # verified_stops = self._extract_verified_stops(verified_trips)
 
         # merge data
-        batch_data = pd.merge(verified_stops, operational_data, on='')
-        batch_data = pd.merge(batch_data, vehicle_information, on='')
+        batch_data = verified_trips.merge(operational_data, how='left',
+                                          right_on='Driver.ID', left_on='DriverID')
 
         # store processed data locally
         if not os.path.exists(config['processed_data_directory']):
@@ -47,14 +67,17 @@ class DataProcessor:
         self.combined_data = pd.concat([self.combined_data, batch_data], ignore_index=True)
         self.combined_data.to_excel(config['processed_data_directory'] + 'combined_data.xlsx', index=False)
 
+        return verified_stops, operational_data, batch_data
+
 
 if __name__ == '__main__':
     processor = DataProcessor()
-    processor.process_data(batch_num=1)
-    processor.process_data(batch_num=2)
-    processor.process_data(batch_num=3)
-    processor.process_data(batch_num=4)
-    processor.process_data(batch_num=5)
-    processor.process_data(batch_num=6)
-    processor.process_data(batch_num=7)
-    processor.process_data(batch_num=8)
+    verified_stops, operational_data, batch_data = processor.process_data(batch_num=1)
+    # processor.process_data(batch_num=1)
+    # processor.process_data(batch_num=2)
+    # processor.process_data(batch_num=3)
+    # processor.process_data(batch_num=4)
+    # processor.process_data(batch_num=5)
+    # processor.process_data(batch_num=6)
+    # processor.process_data(batch_num=7)
+    # processor.process_data(batch_num=8)
