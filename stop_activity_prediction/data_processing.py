6ed3f146fca889b01cdc43a7e4a7a511a1c5793f
Raymond Low <iamraymondlow@gmail.com>
2021-06-17 01:56:04 +0800

    remove irrelevant columns and included documentation

--- stop_activity_prediction/data_processing.py
+++ stop_activity_prediction/data_processing.py
@@ -14,9 +14,23 @@ class DataProcessor:
     operational survey and vehicle information.
     """
     def __init__(self):
+        """
+        Initialises the class object with an empty dataframe to store the combined data for each batch.
+        """
         self.combined_data = pd.DataFrame()
 
     def _load_verified_trips(self, batch_num):
+        """
+        Loads the verified trips data for a particular batch and removes the irrelevant columns.
+
+        Parameters:
+            batch_num: int
+                Contains the batch number.
+
+        Return:
+            verified_trips[retained_columns]: pandas.DataFrame
+                Contains the verified trips information for a particular batch.
+        """
         with open(config['verified_stop_directory'].format(batch_num=batch_num)) as f:
             verified_trips = json.load(f)
         verified_trips = pd.json_normalize(verified_trips)
@@ -26,31 +40,59 @@ class DataProcessor:
 
         return verified_trips[retained_columns]
 
-    def _load_operational_survey(self, batch_num):
+    def load_operation_survey(self, batch_num):
+        """
+        Loads the operation survey for a particular batch and removes the irrelevant columns.
+
+        Parameters:
+            batch_num: int
+                Contains the batch number.
+
+        Return:
+            operation_data[retained_columns]: pandas.DataFrame
+                Contains the operation survey data for a particular batch.
+        """
         # load operational survey
         with open(config['operation_survey_directory'].format(batch_num=batch_num)) as f:
-            operational_data = json.load(f)
-        operational_data = pd.json_normalize(operational_data)
+            operation_data = json.load(f)
+        operation_data = pd.json_normalize(operation_data)
 
         # filter important features
         important_features = ['FrequentPlaces', 'Commodity', 'SpecialCargo', 'Company.Type', 'Industry',
                               'Driver.ID']
         retained_columns = [column
-                            for column in operational_data.columns
+                            for column in operation_data.columns
                             for feature in important_features
                             if feature in column]
 
-        return operational_data[retained_columns]
+        return operation_data[retained_columns]
 
     def _extract_verified_stops(self, verified_trips):
+        """
+        Extracts the verified stop information based on the verified trips.
+
+        Parameters:
+            verified_trips: pandas.DataFrame
+                Contains the verified trip information for a particular batch
+        """
         return None
 
     def process_data(self, batch_num):
+        """
+        Performs data fusion and subsequent processing for the verified trips and operation survey data
+        for a particular batch. The processed batch data and combined dataset is saved in the local directory.
+
+        Parameters:
+            batch_num: int
+                Contains the batch number.
+        """
+        print('Processing Batch {} Data...'.format(batch_num))
+
         # import verified trip information
         verified_trips = self._load_verified_trips(batch_num)
 
-        # import operational survey data
-        operational_data = self._load_operational_survey(batch_num)
+        # import operation survey data
+        operational_data = self._load_operation_survey(batch_num)
 
         # extract verified stop information
         # verified_stops = self._extract_verified_stops(verified_trips)
@@ -59,7 +101,7 @@ class DataProcessor:
         batch_data = verified_trips.merge(operational_data, how='left',
                                           right_on='Driver.ID', left_on='DriverID')
 
-        # store processed data locally
+        # store processed batch data and combined data locally
         if not os.path.exists(config['processed_data_directory']):
             os.makedirs(config['processed_data_directory'])
 
@@ -67,17 +109,14 @@ class DataProcessor:
         self.combined_data = pd.concat([self.combined_data, batch_data], ignore_index=True)
         self.combined_data.to_excel(config['processed_data_directory'] + 'combined_data.xlsx', index=False)
 
-        return verified_stops, operational_data, batch_data
-
 
 if __name__ == '__main__':
     processor = DataProcessor()
-    verified_stops, operational_data, batch_data = processor.process_data(batch_num=1)
-    # processor.process_data(batch_num=1)
-    # processor.process_data(batch_num=2)
-    # processor.process_data(batch_num=3)
-    # processor.process_data(batch_num=4)
-    # processor.process_data(batch_num=5)
-    # processor.process_data(batch_num=6)
-    # processor.process_data(batch_num=7)
-    # processor.process_data(batch_num=8)
+    processor.process_data(batch_num=1)
+    processor.process_data(batch_num=2)
+    processor.process_data(batch_num=3)
+    processor.process_data(batch_num=4)
+    processor.process_data(batch_num=5)
+    processor.process_data(batch_num=6)
+    processor.process_data(batch_num=7)
+    processor.process_data(batch_num=8)
