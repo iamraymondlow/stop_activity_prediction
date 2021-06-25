b1c6065b1d869edccc85ca9de28370c5a1b7a876
Raymond Low <iamraymondlow@gmail.com>
2021-06-26 00:15:43 +0800

    included functions to calculate stop statistics

--- stop_activity_prediction/data_exploration.py
+++ stop_activity_prediction/data_exploration.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import os
 import matplotlib.pyplot as plt
+import seaborn as sns
 from pathlib import Path
 
 # load config file
@@ -22,8 +23,16 @@ class DataExplorer:
         stop_data = pd.read_excel(config['processed_data_directory'] + 'combined_stop_data.xlsx')
         self.trip_data = trip_data
         self.stop_data = stop_data
+        self.activity_types = ['DeliverCargo', 'PickupCargo', 'Other', 'Shift', 'ProvideService',
+                               'OtherWork', 'Meal', 'DropoffTrailer', 'PickupTrailer', 'Fueling',
+                               'Personal', 'Passenger', 'Resting', 'Queuing', 'DropoffContainer',
+                               'PickupContainer', 'Fail', 'Passenger']
 
-    def _plot_bar_graph_multicol(self, feature_name, title):
+        # create data analysis folder if not found
+        if not os.path.exists(config['data_analysis_directory']):
+            os.makedirs(config['data_analysis_directory'])
+
+    def _plot_bar_graph_multicol(self, feature_name, title, data='trip'):
         """
         Plots a bar graph for a user-defined feature that is stored over multiple columns.
 
@@ -32,9 +41,18 @@ class DataExplorer:
                 Contains the feature substring that can be found in the columns of interest.
             title: str
                 Contains the title indicated on the bar graph.
+            data: str
+                Indicates whether we are interested in trip data or stop data
         """
-        feature_columns = [column for column in self.trip_data.columns if feature_name in column]
-        feature_data = self.trip_data[feature_columns]
+        if data == 'trip':
+            feature_columns = [column for column in self.trip_data.columns if feature_name in column]
+            feature_data = self.trip_data[feature_columns]
+        elif data == 'stop':
+            feature_columns = [column for column in self.stop_data.columns if feature_name in column]
+            feature_data = self.stop_data[feature_columns]
+        else:
+            raise ValueError('Data type {} is not supported. data accepts "stop" or "trip".'.format(data))
+
         feature_sum = feature_data.sum(axis=0)
 
         feature_sum.plot(kind='bar')
@@ -44,7 +62,7 @@ class DataExplorer:
         plt.title(title)
         plt.show()
 
-    def _plot_bar_graph_singlecol(self, column_name, title):
+    def _plot_bar_graph_singlecol(self, column_name, title, data='trip'):
         """
         Plots a bar graph for a user-defined feature that is stored in one column.
 
@@ -53,8 +71,15 @@ class DataExplorer:
                 Contains the column name of the feature of interest.
             title: str
                 Contains the title indicated on the bar graph.
+            data: str
+                Indicates whether we are interested in trip data or stop data
         """
-        feature_type = self.trip_data[column_name].value_counts()
+        if data == 'trip':
+            feature_type = self.trip_data[column_name].value_counts()
+        elif data == 'stop':
+            feature_type = self.stop_data[column_name].value_counts()
+        else:
+            raise ValueError('Data type {} is not supported. data accepts "stop" or "trip".'.format(data))
         feature_type.plot(kind='bar')
         plt.ylabel('Frequency')
         plt.title(title)
@@ -94,14 +119,53 @@ class DataExplorer:
         # industry type breakdown
         self._plot_bar_graph_multicol('Industry', 'Industry Type')
 
+    def _extract_activity_type(self, series_row):
+        """
+        Extracts and concatenates the activities conducted at a particular stop as a single string
+
+        Parameters:
+            series_row: pandas.Series
+                Contains the attributes of a stop.
+
+        Return:
+            activity_str: str
+                Contains the activities conducted at a particular stop in the form of a string.
+        """
+        activity_list = [key.replace('Activity.', '')
+                         for key, value in series_row.items()
+                         if ('Activity.' in key) and (value == 1)]
+        activity_str = ','.join(activity_list)
+        return activity_str
+
     def calculate_stop_statistics(self):
         """
         Calculates different statistics related to stop data.
         """
-        return None
+        # calculate number of unique stops
+        print('Number of unique stops: {}'.format(len(self.stop_data['StopID'].unique())))
+
+        # place type breakdown
+        self._plot_bar_graph_multicol('PlaceType', 'Place Type', data='stop')
+
+        # activity type breakdown
+        self._plot_bar_graph_multicol('Activity', 'Activity Type', data='stop')
+
+        # activity type subset breakdown
+        self.stop_data['ActivityType'] = self.stop_data.apply(self._extract_activity_type, axis=1)
+        self.stop_data['ActivityType'].value_counts().to_excel(
+            config['data_analysis_directory'] + 'activity_breakdown.xlsx',
+            index=True)
+
+        # stop duration distribution based on activity types
+        filtered_stop_data = self.stop_data[self.stop_data['ActivityType'].isin(self.activity_types)]
+        sns.boxplot(y='Duration', x='ActivityType', data=filtered_stop_data, showfliers=False)
+        plt.ylabel('Stop Duration (s)')
+        plt.xticks(rotation=90)
+        plt.show()
 
 
 if __name__ == '__main__':
     explorer = DataExplorer()
-    explorer.calculate_trip_statistics()
+    # explorer.calculate_trip_statistics()
     explorer.calculate_stop_statistics()
+    stop_data = explorer.stop_data
